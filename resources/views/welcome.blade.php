385a9bfe93f3bbf76b33b4209382243a44470301
Andolon-M <aemendez-l@outlook.com>
2024-03-29 02:12:21 -0500

    feat: :construction: correccion de error en la navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,13 +27,13 @@
                 @endfor
 
                 <!-- Slider indicators -->
-                <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2 z-50 flex space-x-3 rtl:space-x-reverse">
+                <div class="absolute bottom-5 left-1/2 transform -translate-x-1/2 z-40 flex space-x-3 rtl:space-x-reverse">
                     @for($i = 1; $i <= 8; $i++) <button type="button" class="w-3 h-3 rounded-full {{ $i == 1 ? 'bg-gray-200' : 'bg-gray-200' }}" aria-current="{{ $i == 1 ? 'true' : 'false' }}" aria-label="Slide {{ $i }}" data-carousel-slide-to="{{ $i - 1 }}"></button>
                         @endfor
                 </div>
 
                 <!-- Slider controls -->
-                <button type="button" class="absolute top-0 start-0 z-50 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <button type="button" class="absolute top-0 start-0 z-40 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
                     <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
@@ -41,7 +41,7 @@
                         <span class="sr-only">Previous</span>
                     </span>
                 </button>
-                <button type="button" class="absolute top-0 end-0 z-50 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <button type="button" class="absolute top-0 end-0 z-40 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
                     <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
                         <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
